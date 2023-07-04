9f82b33e69506dcef9c60f74b1c95fc30ef9141e
Uratilim <127127998+Uratilim@users.noreply.github.com>
2023-07-04 14:46:24 -0700

    addded team entity

---
+++ src/Entity/Team.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TeamRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TeamRepository::class)]
+class Team
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nickname = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $year_of_est = null;
+
+    #[ORM\Column(length: 1000, nullable: true)]
+    private ?string $motto = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getNickname(): ?string
+    {
+        return $this->nickname;
+    }
+
+    public function setNickname(string $nickname): static
+    {
+        $this->nickname = $nickname;
+
+        return $this;
+    }
+
+    public function getYearOfEst(): ?\DateTimeInterface
+    {
+        return $this->year_of_est;
+    }
+
+    public function setYearOfEst(\DateTimeInterface $year_of_est): static
+    {
+        $this->year_of_est = $year_of_est;
+
+        return $this;
+    }
+
+    public function getMotto(): ?string
+    {
+        return $this->motto;
+    }
+
+    public function setMotto(?string $motto): static
+    {
+        $this->motto = $motto;
+
+        return $this;
+    }
+}
